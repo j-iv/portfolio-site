1ae68c862e0be296e1fe6842b15bff76ced691f7
JishnuV <jishnu.v@target.com>
2023-01-08 00:22:34 +0530

    added github link

--- components/navBar.js
+++ components/navBar.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
-import styles from '../styles/NavBar.module.css'
+import styles from "../styles/NavBar.module.css";
 import { Roboto } from "@next/font/google";
+import { useState } from "react";
+import Image from "next/image";
 
 const roboto = Roboto({
   weight: "400",
   subsets: ["latin"],
 });
-
 
 const navLinks = [
   {
@@ -23,16 +24,34 @@ const navLinks = [
   },
 ];
 export default function NavBar() {
+  const [activeIdx, setActiveIdx] = useState(-1);
   return (
     <nav className={`${styles.nav_bar} ${roboto.className}`}>
-      <div>
-        Jishnu&apos;s Site
-      </div>
+      <span className={styles.nav_bar_left}>
+        <span>
+          <a
+            href={`https://github.com/j-iv`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image src="/github.svg" alt="Github logo" width={20} height={20} />
+          </a>
+        </span>
+        <span>Jishnu&apos;s Site</span>
+      </span>
+
       <ul className={styles.nav_itemList}>
         {navLinks.map((link, idx) => {
           return (
-            <li className={styles.nav_item} key={idx}>
-              <Link href={link.path}>{link.name} </Link>
+            <li
+              className={`${styles.nav_item} ${
+                idx == activeIdx ? styles.nav_item_active : ""
+              }`}
+              key={idx}
+            >
+              <Link href={link.path} onClick={() => setActiveIdx(idx)}>
+                {link.name}{" "}
+              </Link>
             </li>
           );
         })}
